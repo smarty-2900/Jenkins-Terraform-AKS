20af91f6035b232e28710c70049a1141ea508ec2
smarty-2900 <115106654+smarty-2900@users.noreply.github.com>
2023-09-05 10:25:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,33 @@
 variable "resource_group_name" {
   type        = string
   description = "RG name in Azure"
+  default = "xyz"
 }
 variable "location" {
   type        = string
   description = "Resources location in Azure"
+  default = "xyz"
+
 }
 variable "cluster_name" {
   type        = string
   description = "AKS name in Azure"
+  default = "xyz"
 }
 variable "kubernetes_version" {
   type        = string
   description = "Kubernetes version"
+  default = "xyz"
 }
 variable "system_node_count" {
   type        = number
   description = "Number of AKS worker nodes"
+  default = "xyz"
+
 }
 variable "acr_name" {
   type        = string
   description = "ACR name"
+  default = "xyz"
+
 }
